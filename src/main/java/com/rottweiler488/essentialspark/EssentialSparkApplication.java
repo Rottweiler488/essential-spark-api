f81d1058e996dcf7cfa8c5ef826337358858e0a5
Rottweiler488 <rottweiler488@gmail.com>
2026-06-21 17:30:27 +0300

    local hello apllication Changed gradle version Added SpringBoot, Service, DTO, Controller Added POST and GET

---
+++ src/main/java/com/rottweiler488/essentialspark/EssentialSparkApplication.java
@@ -0,0 +1,11 @@
+package com.rottweiler488.essentialspark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssentialSparkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EssentialSparkApplication.class, args);
+    }
+}
---
+++ src/test/java/com/rottweiler488/essentialspark/MainTest.java
@@ -0,0 +1,13 @@
+package com.rottweiler488.essentialspark;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    @Test
+    public void simpleTest() {
+        String result = "Hello";
+        assertEquals("Hello", result);
+    }
+}
