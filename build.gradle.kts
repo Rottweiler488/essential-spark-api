acb9d43d2acdc1f5f71ed505df6fbf3be9e6d571
Rottweiler488 <rottweiler488@gmail.com>
2026-06-22 17:02:20 +0300

    add checkstyle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.5.3"
 	id("io.spring.dependency-management") version "1.1.7"
+	checkstyle
 }
 
 group = "com.rottweiler488"
@@ -15,6 +16,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+checkstyle {
+	toolVersion = "13.6.0"
 }
 
 tasks.test {
